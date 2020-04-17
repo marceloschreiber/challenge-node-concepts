5628c0fc9a3e7c3c66b05560aaa23ef49741aa57
Marcelo Fernandes <marcelo.schreiber@gmail.com>
2020-04-17 01:12:55 -0300

    Implement challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const httpStatus = require("http-status-codes");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +11,82 @@ app.use(cors());
 
 const repositories = [];
 
+function getRepositoryIndex(id) {
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    throw new Error("Repository not found");
+  }
+
+  return repositoryIndex;
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.status(httpStatus.CREATED).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  try {
+    const repositoryIndex = getRepositoryIndex(id);
+
+    const repository = {
+      ...repositories[repositoryIndex],
+      title,
+      url,
+      techs,
+    };
+
+    return response.json(repository);
+  } catch (err) {
+    return response.status(httpStatus.BAD_REQUEST).json(err.message);
+  }
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  try {
+    const repositoryIndex = getRepositoryIndex(id);
+    repositories.splice(repositoryIndex, 1);
+
+    return response.status(httpStatus.NO_CONTENT).send();
+  } catch (err) {
+    return response.status(httpStatus.BAD_REQUEST).json(err.message);
+  }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  try {
+    const repositoryIndex = getRepositoryIndex(id);
+    const repository = repositories[repositoryIndex];
+
+    const repositoryWithLikeIncremented = {
+      ...repository,
+      likes: repository.likes + 1,
+    };
+
+    repositories[repositoryIndex] = repositoryWithLikeIncremented;
+
+    return response.json(repositoryWithLikeIncremented);
+  } catch (err) {
+    return response.status(httpStatus.BAD_REQUEST).json(err.message);
+  }
 });
 
 module.exports = app;
